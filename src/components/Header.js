c6ade8b2023867f4b6ec286fa358b60bd4272a47
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2023-01-27 17:50:49 -0800

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from "react";
+// Function string array
+import Typed from "react-typed";
 
 const Header = () => {
   return (
     <div className="header-warper">
       <div className="main-info">
         <h1>web developer and websites promotions</h1>
+        <Typed
+          className="typed-text"
+          strings={[
+            "Software Engineer",
+            "Software Developer",
+            "Front-End Developer",
+            "Web Development",
+            "Web Design",
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+        />
       </div>
     </div>
   );
