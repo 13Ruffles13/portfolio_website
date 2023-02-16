401c34524bb37f512577376a20e426450b34b7d9
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2023-02-16 14:43:35 -0800

    newSubBranch | contactInfo

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ const Header = () => {
           backSpeed={60}
           loop
         />
-        <a href="#" className="btn-main-offer"> Contact me</a>
+        <a href="#contactMe" className="btn-main-offer"> Contact me</a>
       </div>
     </div>
   );
